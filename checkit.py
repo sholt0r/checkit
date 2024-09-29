78981bdfa2820a85bead47f764340d2c995be334
sholt0r <sholtor@kobu.au>
2024-09-29 08:04:35 +0000

    Updated requests.txt

--- checkit.py
+++ checkit.py
@@ -4,22 +4,25 @@ import os
 import discord
 from discord.ext import commands
 import requests as re
-import lqa
+import lqa.lqa as lqa
 
 d_token = os.getenv('D_TOKEN')
 s_token = os.getenv('S_TOKEN')
 s_api_url = os.getenv('S_API_URL')
 
-def sendRequest(api_url, token, s_func):
+def sendRequest(api_url, token, s_func, port=7777,):
     headers = {"Content-Type":"application/json", "Authorization":f"Bearer {token}"}
     json = {"function":f"{s_func}"}
     try:
-        response = re.post(api_url, headers=headers, json=json)
+        response = re.post(f"https://{api_url}:{port}/api/v1", headers=headers, json=json)
         if response.status_code not in (200, 204):
+            print(f"INFO: Status code {response.status_code}")
             return False
         if s_func == "QueryServerState":
+            print("INFO: Query command successful")
             return response.json()['data']['serverGameState']
         if s_func == "Shutdown":
+            print("INFO: Shutdown command successful")
             return True
     except:
         return False
@@ -30,24 +33,24 @@ bot = commands.Bot(command_prefix='ficsit ', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Logged on as {bot.user} (ID: {bot.user.id})")
+    print(f"INFO: Logged on as {bot.user} (ID: {bot.user.id})")
 
 @bot.hybrid_command()
 async def status(ctx):
-    print("Status command issued.")
+    print("CMD: Status command issued.")
     status = sendRequest(s_api_url, s_token, "QueryServerState")
     if not status:
-        lstate = lqa.poll_server_state(f"S_API_URL")
+        lstate = lqa.poll_server_state(f"{s_api_url}")
         await ctx.send(f"The server state currently {lstate['state']}")
         return
     await ctx.send(f"Active Session: {status['activeSessionName']}\nNumber of Players: {status['numConnectedPlayers']}/{status['playerLimit']}\nTech Tier: {status['techTier']}")
 
 @bot.hybrid_command()
 async def restart(ctx):
-    print("Restart command issued.")
+    print("CMD: Restart command issued.")
     restart = sendRequest(s_api_url, s_token, "Shutdown")
     if not restart:
-        lstate = lqa.poll_server_state(f"S_API_URL")
+        lstate = lqa.poll_server_state(f"{s_api_url}")
         await ctx.send(f"The server state currently {lstate['state']}")
         return
     await ctx.send("Restarting server.")
