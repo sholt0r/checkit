37d9f95e5de9fa9e9d733c2c6e24fe90c49b4f6f
sholt0r <sholtor@kobu.au>
2024-10-06 14:13:50 +0000

    Updated lwapi submodule connection

--- checkit.py
+++ checkit.py
@@ -1,31 +1,21 @@
 #!/usr/local/bin/python3.12
 
 import os
+import logging
 import discord
 from discord.ext import commands
-import requests as re
-import lqa.lqa as lqa
+import lwapi.lqa as lqa
+import lwapi.hta as hta
 
-d_token = os.getenv('D_TOKEN')
-s_token = os.getenv('S_TOKEN')
-s_api_url = os.getenv('S_API_URL')
+from dotenv import load_dotenv
+load_dotenv()
 
-def sendRequest(api_url, token, s_func, port=7777,):
-    headers = {"Content-Type":"application/json", "Authorization":f"Bearer {token}"}
-    json = {"function":f"{s_func}"}
-    try:
-        response = re.post(f"https://{api_url}:{port}/api/v1", headers=headers, json=json)
-        if response.status_code not in (200, 204):
-            print(f"INFO: Status code {response.status_code}")
-            return False
-        if s_func == "QueryServerState":
-            print("INFO: Query command successful")
-            return response.json()['data']['serverGameState']
-        if s_func == "Shutdown":
-            print("INFO: Shutdown command successful")
-            return True
-    except:
-        return False
+# Bot constants
+D_TOKEN = os.getenv('D_TOKEN')
+S_TOKEN = os.getenv('S_TOKEN')
+S_API_URL = os.getenv('S_API_URL')
+
+
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -38,21 +28,27 @@ async def on_ready():
 @bot.hybrid_command()
 async def status(ctx):
     print("CMD: Status command issued.")
-    status = sendRequest(s_api_url, s_token, "QueryServerState")
+    lstate = lqa.poll_server_state(f"{S_API_URL}")
+    status = hta.send_http_request(S_API_URL, S_TOKEN, "QueryServerState")
     if not status:
-        lstate = lqa.poll_server_state(f"{s_api_url}")
-        await ctx.send(f"The server state currently {lstate['state']}")
+        await ctx.send(f"The server state currently {lstate}")
         return
     await ctx.send(f"Active Session: {status['activeSessionName']}\nNumber of Players: {status['numConnectedPlayers']}/{status['playerLimit']}\nTech Tier: {status['techTier']}")
 
 @bot.hybrid_command()
+async def lstatus(ctx):
+    print("CMD: Lightweight status command issued.")
+    lstate = lqa.poll_server_state(f"{S_API_URL}")
+    await ctx.send(f"The server state currently \"{lstate['state']}\"")
+
+@bot.hybrid_command()
 async def restart(ctx):
     print("CMD: Restart command issued.")
-    restart = sendRequest(s_api_url, s_token, "Shutdown")
+    lstate = lqa.poll_server_state(f"{S_API_URL}")
+    restart = hta.send_http_request(S_API_URL, S_TOKEN, "Shutdown")
     if not restart:
-        lstate = lqa.poll_server_state(f"{s_api_url}")
-        await ctx.send(f"The server state currently {lstate['state']}")
+        await ctx.send(f"The server state currently {lstate}")
         return
     await ctx.send("Restarting server.")
 
-bot.run(f"{d_token}")
+bot.run(f"{D_TOKEN}")
