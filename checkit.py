c533f3e474a984c6929cd837c3d673f27167c9fe
sholt0r <sholtor@kobu.au>
2024-10-08 06:48:11 +0000

    Removed lstatus command

--- checkit.py
+++ checkit.py
@@ -1,12 +1,18 @@
 #!/usr/local/bin/python3.12
 
 import os
+import sys
 import logging
 import discord
 from discord.ext import commands
 from lwapi import lqa, hta
 
-_log = logging.getLogger("checkit")
+handler = logging.StreamHandler()
+formatter = discord.utils._ColourFormatter()
+logger = logging.getLogger("checkit")
+handler.setFormatter(formatter)
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -22,29 +28,23 @@ bot = commands.Bot(command_prefix='ficsit ', intents=intents)
 
 @bot.event
 async def on_ready():
-    _log.info(f"Logged on as {bot.user} (ID: {bot.user.id})")
+    logger.info(f"Logged on as {bot.user} (ID: {bot.user.id})")
 
 @bot.hybrid_command()
 async def status(ctx):
-    _log.debug("Status command issued.")
-    lstate = lqa.poll_server_state(f"{S_API_URL}")
-    status = hta.send_http_request(S_API_URL, S_TOKEN, "QueryServerState")
+    logger.info("Status command issued.")
+    lstate = lqa.poll_server_state(f"{S_API_URL}", logger)
+    status = hta.send_http_request(S_API_URL, S_TOKEN, "QueryServerState", logger)
     if not status:
         await ctx.send(f"The server state currently {lstate}")
         return
     await ctx.send(f"Active Session: {status['activeSessionName']}\nNumber of Players: {status['numConnectedPlayers']}/{status['playerLimit']}\nTech Tier: {status['techTier']}")
 
 @bot.hybrid_command()
-async def lstatus(ctx):
-    _log.debug("Lightweight status command issued.")
-    lstate = lqa.poll_server_state(f"{S_API_URL}")
-    await ctx.send(f"The server state currently \"{lstate['state']}\"")
-
-@bot.hybrid_command()
 async def restart(ctx):
-    _log.debug("Restart command issued.")
-    lstate = lqa.poll_server_state(f"{S_API_URL}")
-    restart = hta.send_http_request(S_API_URL, S_TOKEN, "Shutdown")
+    logger.info("Restart command issued.")
+    lstate = lqa.poll_server_state(f"{S_API_URL}", logger)
+    restart = hta.send_http_request(S_API_URL, S_TOKEN, "Shutdown", logger)
     if not restart:
         await ctx.send(f"The server state currently {lstate}")
         return
