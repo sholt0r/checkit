259b2572f95b5a2f68fa369a5dbf0f31c8dc08be
sholt0r <sholtor@kobu.au>
2024-10-10 19:45:22 +0000

    Updated logging

--- checkit.py
+++ checkit.py
@@ -1,5 +1,3 @@
-#!/usr/local/bin/python3.12
-
 import os
 import sys
 import logging
@@ -7,17 +5,18 @@ import discord
 from discord.ext import commands
 from lwapi import hta#, lqa
 
-handler = logging.StreamHandler()
-formatter = discord.utils._ColourFormatter()
-logger = logging.getLogger("checkit")
-handler.setFormatter(formatter)
-logger.setLevel(logging.INFO)
-logger.addHandler(handler)
-
-# Bot constants
+# Constants
 D_TOKEN = os.getenv('D_TOKEN')
 S_TOKEN = os.getenv('S_TOKEN')
 S_API_URL = os.getenv('S_API_URL')
+LOGGER_NAME = "checkit"
+
+handler = logging.StreamHandler()
+formatter = discord.utils._ColourFormatter()
+logger = logging.getLogger(LOGGER_NAME)
+handler.setFormatter(formatter)
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -31,7 +30,7 @@ async def on_ready():
 async def status(ctx):
     logger.info("Status command issued.")
     #lstate = lqa.poll_server_state(f"{S_API_URL}", logger)
-    status = hta.send_http_request(S_API_URL, S_TOKEN, "QueryServerState", logger)
+    status = hta.send_http_request(S_API_URL, S_TOKEN, "QueryServerState", LOGGER_NAME)
     if not status:
         #await ctx.send(f"The server state currently {lstate}")
         await ctx.send(f"The server is not responding")
@@ -42,7 +41,7 @@ async def status(ctx):
 async def restart(ctx):
     logger.info("Restart command issued.")
     #lstate = lqa.poll_server_state(f"{S_API_URL}", logger)
-    restart = hta.send_http_request(S_API_URL, S_TOKEN, "Shutdown", logger)
+    restart = hta.send_http_request(S_API_URL, S_TOKEN, "Shutdown", LOGGER_NAME)
     if not restart:
         #await ctx.send(f"The server state currently {lstate}")
         await ctx.send(f"The server is not responding")
