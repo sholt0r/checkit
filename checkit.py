cd7858f2d5a4cb03b0149c163855a2292a3f4eff
sholt0r <sholtor@kobu.au>
2024-10-12 05:25:21 +1100

    Changes for implementing proper server state.

--- checkit.py
+++ checkit.py
@@ -2,14 +2,15 @@ import os
 import sys
 import logging
 import discord
+import asyncio
 from discord.ext import commands
-from common import hta, log
+from common import hta, lqa, log
 
 # Constants
 D_TOKEN = os.getenv('D_TOKEN')
 S_TOKEN = os.getenv('S_TOKEN')
-S_API_HOST = os.getenv('S_API_HOST')
-S_API_PORT = os.getenv('S_PORT', 7777)
+HOST = os.getenv('S_API_HOST')
+PORT = os.getenv('S_PORT', 7777)
 
 logger = log.setup_logger()
 
@@ -24,10 +25,7 @@ async def on_ready():
 @bot.hybrid_command()
 async def status(ctx):
     logger.info("Status command issued.")
-    #lstate = lqa.poll_server_state(S_API_HOST, S_API_PORT, logger)
-    status = hta.send_http_request(S_API_HOST, S_API_PORT, S_TOKEN, "QueryServerState")
     if not status:
-        #await ctx.send(f"The server state currently {lstate}")
         await ctx.send(f"The server is not responding")
         return
     await ctx.send(f"Active Session: {status['activeSessionName']}\nNumber of Players: {status['numConnectedPlayers']}/{status['playerLimit']}\nTech Tier: {status['techTier']}")
@@ -35,10 +33,7 @@ async def status(ctx):
 @bot.hybrid_command()
 async def restart(ctx):
     logger.info("Restart command issued.")
-    #lstate = lqa.poll_server_state(S_API_HOST, S_API_PORT, logger)
-    restart = hta.send_http_request(S_API_HOST, S_API_PORT, S_TOKEN, "Shutdown")
     if not restart:
-        #await ctx.send(f"The server state currently {lstate}")
         await ctx.send(f"The server is not responding")
         return
     await ctx.send("Restarting server.")
