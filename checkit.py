31212a3d322988413da8bde6f10fd776a197b2d8
sholt0r <sholtor@kobu.au>
2024-10-12 16:55:56 +1100

    Changes to common and some alterations to the main program for new state implementation.

--- checkit.py
+++ checkit.py
@@ -1,8 +1,4 @@
-import os
-import sys
-import logging
-import discord
-import asyncio
+import os, sys, logging, discord, asyncio
 from discord.ext import commands
 from common import hta, lqa, log
 
@@ -18,23 +14,26 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='ficsit ', intents=intents)
 
+
 @bot.event
 async def on_ready():
     logger.info(f"Logged on as {bot.user} (ID: {bot.user.id})")
+
 
 @bot.hybrid_command()
 async def status(ctx):
     logger.info("Status command issued.")
     if not status:
-        await ctx.send(f"The server is not responding")
+        await ctx.send("The server is not responding")
         return
     await ctx.send(f"Active Session: {status['activeSessionName']}\nNumber of Players: {status['numConnectedPlayers']}/{status['playerLimit']}\nTech Tier: {status['techTier']}")
+
 
 @bot.hybrid_command()
 async def restart(ctx):
     logger.info("Restart command issued.")
     if not restart:
-        await ctx.send(f"The server is not responding")
+        await ctx.send("The server is not responding")
         return
     await ctx.send("Restarting server.")
 
